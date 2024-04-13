ae60244ce3d1edbe07fe72d284e54ce1b0463314
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-04-13 18:29:43 -0300

    🔧 Updated network-ip.sh
    Changed comment language to English for clarity.
    
    Used environment variable '$HOME' for script path.

--- network/network-ip.sh
+++ network/network-ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Obter o endereço IP usando o comando diretamente
+# Obtain the IP address using the command directly
 ip_address=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}')
 
-python3 /home/esperanca/scripts/network/calculate-network-ip.py "$ip_address"
+python3 $HOME/scripts/network/calculate-network-ip.py "$ip_address"
